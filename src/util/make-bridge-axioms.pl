30ddbd53abda412ab39aa98f61cc4b9d3f6082db
cmungall <cmungall@498ed3e6-e001-1550-e0bd-7271ed0353fc>
2014-06-25 19:47:35 +0000

    new

--- src/util/make-bridge-axioms.pl
+++ src/util/make-bridge-axioms.pl
@@ -6,8 +6,13 @@ my $taxname = shift;
 while(<>) {
     chomp;
     s/(\r\n|\r)/\n/g;
+    if (m@^\[(\S+)\]@) {
+        $type = $1;
+    }
     if (m@id: (\S+)@) {
         $id = $1;
+        #print "$id $type\n";
+        $typem{$id} = $type;
     }
     if (m@^name: (.*)@) {
         $n{$id} = $1;
@@ -17,17 +22,27 @@ while(<>) {
     }
 }
 
-foreach my $id (keys %x) {
+foreach my $id (keys %n) {
+    if ($typem{$id} ne 'Term') {
+        next;
+    }
     my $name = $n{$id};
     print "[Term]\n";
     print "id: $id\n";
     printf('property_value: IAO:0000589 "%s (%s)" xsd:string', $name, $taxname);
     print "\n";
-    print "intersection_of: $x{$id}\n";
-    print "intersection_of: part_of: $taxid\n";
+    print "relationship: only_in_taxon $taxid ! $taxname\n";
+    if ($x{$id}) {
+        print "intersection_of: $x{$id}\n";
+        print "intersection_of: part_of $taxid\n";
+    }
     print "\n";
 }
 
 print "[Typedef]\n";
 print "id: part_of\n";
-print "xref: BFO:0000050\n";
+print "xref: BFO:0000050\n\n";
+
+print "[Typedef]\n";
+print "id: only_in_taxon\n";
+print "xref: RO:0002160\n\n";
